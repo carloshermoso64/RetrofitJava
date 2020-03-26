351eefc50ecac29d29b25f502b3adb7299935e6a
carloshermoso64 <59064594+carloshermoso64@users.noreply.github.com>
2020-03-26 16:03:11 +0100

    Username by Input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,41 +4,59 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello Word");
+        System.out.println("Inserta usuario GitHub");
 
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .build();
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build();
+        //Enter data using BufferReader
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
 
-        GitHubService service = retrofit.create(GitHubService.class);
+        // Reading data using readLine
+        try {
+            String username = reader.readLine();
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
-        Call<List<Repo>> repos = service.listRepos("carloshermoso64");
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build();
 
-        try{
-           List<Repo> result = repos.execute().body();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl("https://api.github.com/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
 
-           for (Repo r: result){
-               System.out.println(r);
-           }
+            GitHubService service = retrofit.create(GitHubService.class);
+
+            Call<List<Repo>> repos = service.listRepos(username);
+
+            try{
+                List<Repo> result = repos.execute().body();
+
+                for (Repo r: result){
+                    System.out.println(r);
+                }
+            }
+            catch (Exception e){
+                System.out.println("EXCEPCION:");
+                System.out.println(e.toString());
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        catch (Exception e){
-            System.out.println("EXCEPCION:");
-            System.out.println(e.toString());
-        }
+
 
     }
 }
